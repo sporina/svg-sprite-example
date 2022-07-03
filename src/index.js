62a6a25815d82dff6f384c8cdfcaeda8f093bdb9
Evgeniy <evgeniykravtsov1992@gmail.com>
2022-07-03 19:15:34 +0300

    add classname for svg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <svg >
-    <use href={`/sprite.svg#example-1`} />
+    <div >
+      <head>
+        <link rel="preload" as="image/svg+xml" href="spriteExample.svg"/>
+      </head>
+    <svg>
+      <use className="yellow" href={`/sprite.svg#example-1`} />
     </svg>
     <svg >
       <use href={`/sprite.svg#example-2`} />
